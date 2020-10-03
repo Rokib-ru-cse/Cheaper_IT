0a667e2e279f757896844a20b139faeb5981e9a2
Rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2020-10-04 01:36:14 +0600

    Updates

---
+++ precache-manifest.b7be8871b6d87be09b0f5c0174940b8c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb36e87ea60504287c7e3fe43c2d66a3",
+    "url": "/Cheaper_IT/index.html"
+  },
+  {
+    "revision": "3beaf648753045c3a5db",
+    "url": "/Cheaper_IT/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "5a214f440725494adb19",
+    "url": "/Cheaper_IT/static/css/main.11db7188.chunk.css"
+  },
+  {
+    "revision": "3beaf648753045c3a5db",
+    "url": "/Cheaper_IT/static/js/2.50d46a06.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Cheaper_IT/static/js/2.50d46a06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a214f440725494adb19",
+    "url": "/Cheaper_IT/static/js/main.d575888a.chunk.js"
+  },
+  {
+    "revision": "981c3bab68bbfbe40f38",
+    "url": "/Cheaper_IT/static/js/runtime-main.fd28beb2.js"
+  },
+  {
+    "revision": "619f0cc8062be2e032aa50e3e238b2a2",
+    "url": "/Cheaper_IT/static/media/aboutpage.619f0cc8.png"
+  },
+  {
+    "revision": "0ef26c4663931cbf02d05bac8c45feb0",
+    "url": "/Cheaper_IT/static/media/android.0ef26c46.jpg"
+  },
+  {
+    "revision": "9ce8aa204a21e69716018fee472b042c",
+    "url": "/Cheaper_IT/static/media/app.9ce8aa20.png"
+  },
+  {
+    "revision": "a53e79c11de68a0e1d1a98a512c3f2ed",
+    "url": "/Cheaper_IT/static/media/digitalmarketing.a53e79c1.jpg"
+  },
+  {
+    "revision": "0f8829c5e5f58d8f4409fda5bbe29368",
+    "url": "/Cheaper_IT/static/media/homepage.0f8829c5.png"
+  },
+  {
+    "revision": "f1efc491797fa96496933731707cf1a9",
+    "url": "/Cheaper_IT/static/media/ios.f1efc491.jpg"
+  },
+  {
+    "revision": "3f166ce9f3666fe60f6c92bd4cad9d5d",
+    "url": "/Cheaper_IT/static/media/logo2.3f166ce9.png"
+  },
+  {
+    "revision": "f045094518e2876301841a9a3ca7dd21",
+    "url": "/Cheaper_IT/static/media/marketing.f0450945.jpg"
+  },
+  {
+    "revision": "76d352ebe448d3847f4e1e286d601093",
+    "url": "/Cheaper_IT/static/media/software.76d352eb.jpg"
+  },
+  {
+    "revision": "426e24f5215079ab20b8aeed47567616",
+    "url": "/Cheaper_IT/static/media/web.426e24f5.png"
+  }
+]);
