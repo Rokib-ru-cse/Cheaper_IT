6c8847f4c7ff15ff235b54c9e45449d056838c2a
Rokib-ru-cse <rokibahmed.ru.cse@gmail.com>
2020-10-04 02:00:36 +0600

    My react website

--- src/Common.jsx
+++ src/Common.jsx
@@ -10,7 +10,7 @@ function Common(props) {
                 <div className="col-10 mx-auto">
                     <div className="row">
                         <div className="col-md-6 pt-6 pt-lg-0 order-2 order-lg-1 d-flex justify-content-center flex-column">
-                            <h1>{props.name}<strong className="brand-name"> Ceaper IT</strong> </h1>
+                            <h1>{props.name}<strong className="brand-name"> Cheaper IT</strong> </h1>
                             <h2 className="my-3">We are the team od talented developers making websites</h2>
                             <div className="mt-3">
                                 <NavLink to={props.visit} className="btn-get-started">{props.btn_name}</NavLink>
